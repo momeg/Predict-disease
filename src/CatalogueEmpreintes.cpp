5b29710acb1aee6bb81f2cb15a968597ac386460
unknown <lsaos@IF501-210-11.insa-lyon.fr>
2018-05-28 15:50:48 +0200

    AttributDouble

---
+++ src/CatalogueEmpreintes.cpp
@@ -0,0 +1,200 @@
+#include "CatalogueEmpreintes.hpp"
+#include "AttributDouble.hpp"
+#include "AttributString.hpp"
+#include "DefinitionAttributId.hpp"
+#include "DefinitionAttributString.hpp"
+#include "DefinitionAttributDouble.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+//----------------------------------------------------------------- PUBLIC
+
+bool CatalogueEmpreintes::chargerFichier(const string &cheminFichier)
+{
+	ifstream fichier;
+	fichier.open(cheminFichier);
+
+	if (fichier)
+	{ //Si on peut accéder au fichier indiqué
+		string uneLigne;
+
+		if (getline(fichier, uneLigne))
+		{ //La ligne pour donner le format du document
+			istringstream infosLigne;
+			infosLigne.str(uneLigne);
+
+			string info;
+			vector<string> ordreAttributs;
+			while (getline(infosLigne, info, ';')) {
+				ordreAttributs.push_back(info);
+			}
+
+			int numeroAttribut;
+			ListeAttributs * listeAttributs;
+			int index;
+
+			int id;
+			bool idNonExistant;
+
+			string maladie;
+			bool malade;
+
+			while (getline(fichier, uneLigne)) {  //On lit une ligne
+				listeAttributs = new ListeAttributs();
+				numeroAttribut = 0;
+
+				infosLigne.clear();
+				infosLigne.str(uneLigne);
+
+				idNonExistant = true;
+				malade = false;
+
+				while (getline(infosLigne, info, ';')) {  //On lit chaque information de la ligne décrivant l'empreinte
+					if (ordreAttributs[numeroAttribut] == "Disease") {
+						malade = true;
+						maladie = info;
+					}
+					else if (idNonExistant) {            //Si on retrouve deux fois la même id, c'est que l'on souhaite uniquement spécifier une autre maladie pour le client
+														// le reste n'a pas besoin d'être modifier
+						index = indexAttribut(ordreAttributs[numeroAttribut]);
+						if (definitionAttributs[index]->getType() == ATTRIBUT_DOUBLE) {
+							listeAttributs->push_back(new AttributDouble(stod(info)));
+						}
+						else if (definitionAttributs[index]->getType() == ATTRIBUT_STRING) {
+							listeAttributs->push_back(new AttributString(info));
+						}
+						else if (definitionAttributs[index]->getType() == ATTRIBUT_ID) {
+							id = stoi(info);
+							if (empreintes.find(id) != empreintes.end()) {
+								idNonExistant = false;
+							}
+						}
+					}
+					++numeroAttribut;
+				}
+
+				if (idNonExistant) {
+					empreintes.insert(make_pair(id, Empreinte(id, *listeAttributs)));
+				}
+				if (malade && (empreintes.find(id) != empreintes.end())) {
+					empreintes[id].ajouterMaladie(maladie);
+				}
+			}
+			return true;
+		}
+		else
+		{
+			cerr << "Le document est vide";
+			return false;
+		}
+	}
+	else
+	{
+		cerr << "Le fichier n'a pas été trouvé" << endl;
+		return false;
+	}
+}
+
+bool CatalogueEmpreintes::chargerDefinitionAttributs(const string &cheminFichier)
+{
+	ifstream fichier;
+	fichier.open(cheminFichier);
+
+	if (fichier)
+	{ //Si on peut accéder au fichier indiqué
+		string uneLigne;
+
+		if (getline(fichier, uneLigne))
+		{ //La ligne pour donner le format du document
+			istringstream infosLigne;
+			infosLigne.str(uneLigne);
+
+			string info;
+			string attribut;
+			string type;
+
+			getline(infosLigne, info, ';');
+
+			if (info == "AttributeName") {   //L'attribut est avant le type
+				while (getline(fichier, uneLigne)) {
+					infosLigne.clear();
+					infosLigne.str(uneLigne);
+
+					getline(infosLigne, attribut, ';');
+					getline(infosLigne, type);
+
+					ajouterUneDefinitionAttribut(attribut, type);
+				}
+			}
+			else {                           //Le type est avant l'attribut
+				while (getline(fichier, uneLigne)) {
+					infosLigne.clear();
+					infosLigne.str(uneLigne);
+
+					getline(infosLigne, type, ';');
+					getline(infosLigne, attribut);
+
+					ajouterUneDefinitionAttribut(attribut, type);
+				}
+			}
+			return true;
+		}
+		else
+		{
+			cerr << "Le document est vide";
+			return false;
+		}
+	}
+	else
+	{
+		cerr << "Le fichier n'a pas été trouvé" << endl;
+		return false;
+	}
+}
+
+const CatalogueEmpreintes::ListeEmpreintes& CatalogueEmpreintes::getEmpreintes() const
+{
+	return empreintes;
+}
+
+const ListeDefinitionAttributs& CatalogueEmpreintes::getDefinitionAttribut() const
+{
+	return definitionAttributs;
+}
+
+//-------------------------------------------- Constructeurs - destructeur
+CatalogueEmpreintes::CatalogueEmpreintes()
+{
+}
+
+//----------------------------------------------------------------- PRIVE
+
+void CatalogueEmpreintes::ajouterUneDefinitionAttribut(const string& attribut, const string& type)
+{
+	if (type == "double") {
+		definitionAttributs.push_back(new DefinitionAttributDouble(attribut));
+	}
+	else if (type == "string") {
+		definitionAttributs.push_back(new DefinitionAttributString(attribut));
+	}
+	else if (type == "ID") {
+		definitionAttributs.push_back(new DefinitionAttributId(attribut));
+	}
+}
+
+int CatalogueEmpreintes::indexAttribut(const string& attribut)
+{
+	for (size_t i = 0; i < definitionAttributs.size(); i++)
+	{
+		if (definitionAttributs[i]->getNom() == attribut)
+		{
+			return i;
+		}
+	}
+	return 0;
+}
---
+++ src/CatalogueEmpreintes.hpp
@@ -0,0 +1,47 @@
+#ifndef CATALOGUEEMPREINTES_HPP
+#define CATALOGUEEMPREINTES_HPP
+
+
+#include "Empreinte.hpp"
+#include "DefinitionAttribut.hpp"
+
+#include <unordered_map>
+#include<vector>
+
+class CatalogueEmpreintes
+{
+public:
+    typedef unordered_map <int, Empreinte> ListeEmpreintes;
+
+public:
+    CatalogueEmpreintes();
+
+public:
+    
+    bool chargerFichier(const string& cheminFichier);
+    // Mode d'emploi : récupère l'adresse du fichier que l'utilisateur souhaite charger
+    // La fonction récupère toutes les informations du fichier pour créer une nouvelle empreinte qui sera ajouter dans
+    // la liste empreinte
+    // Au préalable, il faut IMPERATIVEMENT que l'utilisateur ait fait tourner la fonction chargerDefinitionAttribut
+    // afin de connaitre le type de chaque attribut
+
+    bool chargerDefinitionAttributs(const string& cheminFichier);
+    // Mode d'emploi : récupère l'adresse du fichier de méta-donnée fourni par l'utilisateur
+    // La fonction récupère le type de chaque attribut qui constitueront les empreintes que donnera cet utilisateur
+    // puis les stocke dans definitionAttributs 
+
+    const ListeEmpreintes& getEmpreintes() const;
+    // Mode d'emploi : renvoie la liste empreintes
+
+    const ListeDefinitionAttributs& getDefinitionAttribut() const;
+
+private:
+    void ajouterUneDefinitionAttribut(const string& attribut, const string& type);
+
+    int indexAttribut(const string& attribut);
+
+    ListeEmpreintes empreintes;
+    ListeDefinitionAttributs definitionAttributs;
+};
+
+#endif // CATALOGUEEMPREINTES_HPP
